1e60e91a0449bd1b13e5afad01faeb89477ca545
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-11-29 16:08:28 -0300

    test: create more use cases

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -8,4 +8,31 @@ describe('Object to query string', () => {
 
     expect(queryString(object)).toBe('name=Renan&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const object = {
+      name: 'Renan',
+      profession: 'developer',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(object)).toBe(
+      'name=Renan&profession=developer&abilities=JS,TDD',
+    );
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const object = {
+      name: 'Renan',
+      profession: 'developer',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(object);
+    }).toThrowError();
+  });
 });
