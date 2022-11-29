40ce1ca325cc6d5e36c1e0f9d37b9af2b0fe75ec
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-11-29 15:34:41 -0300

    refactor: get key and value of array

--- src/lib/querString.js
+++ src/lib/querString.js
@@ -1,7 +1,5 @@
-module.exports.queryString = (obj) => {
-    const entries = Object.entries(obj).map(item => {
-        return `${item[0]}=${item[1]}`
-    })
-
-    return entries.join('&')
-}
+module.exports.queryString = obj => {
+  return Object.entries(obj)
+    .map(([key, val]) => `${key}=${val}`)
+    .join('&');
+};
