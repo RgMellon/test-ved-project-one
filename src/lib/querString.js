60accc7e542df3ae305a17b285ca20e822470202
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-11-29 16:08:05 -0300

    refactor: create keyValueToString

--- src/lib/querString.js
+++ src/lib/querString.js
@@ -1,5 +1,10 @@
+const keyValueToString = ([key, val]) => {
+  if (typeof val == 'object' && !Array.isArray(val)) {
+    throw new Error('Please check your params');
+  }
+  return `${key}=${val}`;
+};
+
 module.exports.queryString = obj => {
-  return Object.entries(obj)
-    .map(([key, val]) => `${key}=${val}`)
-    .join('&');
+  return Object.entries(obj).map(keyValueToString).join('&');
 };
