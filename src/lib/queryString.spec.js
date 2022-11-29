d2e32e1cfe5c799d443b8a2dd1d08fca40e007c0
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-11-29 15:35:02 -0300

    chore: prettier format

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,11 +1,11 @@
-const {queryString} = require('./querString')
+const { queryString } = require('./querString');
 describe('Object to query string', () => {
-    it('should create a valid query string when an object is provided', () => {
-        const object = {
-            name: 'Renan',
-            profession: 'developer'
-        }
+  it('should create a valid query string when an object is provided', () => {
+    const object = {
+      name: 'Renan',
+      profession: 'developer',
+    };
 
-        expect(queryString(object)).toBe('name=Renan&profession=developer')
-    })
-})
+    expect(queryString(object)).toBe('name=Renan&profession=developer');
+  });
+});
