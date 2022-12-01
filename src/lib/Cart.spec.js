73284ee136a573eb2c2eb08e81e2a43bbf2aed38
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-12-01 15:10:35 -0300

    refactor: using Dinero to work with money

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -19,7 +19,7 @@ describe('Cart', () => {
 
   describe('getTotal()', () => {
     it('should return 0 when getTotal() is executed in a newly created instance', () => {
-      expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
     it('should multiply quantity and price and receive the total amount', () => {
@@ -29,14 +29,14 @@ describe('Cart', () => {
       };
 
       cart.add(item);
-      expect(cart.getTotal()).toEqual(70776);
+      expect(cart.getTotal().getAmount()).toEqual(70776);
     });
 
     it('should ensure no more than one product exists at a time', () => {
       cart.add({ product, quantity: 2 });
       cart.add({ product, quantity: 1 });
 
-      expect(cart.getTotal()).toEqual(35388);
+      expect(cart.getTotal().getAmount()).toEqual(35388);
     });
 
     it('should update total when a product gets included and then removed', () => {
@@ -45,7 +45,7 @@ describe('Cart', () => {
 
       cart.remove(product);
 
-      expect(cart.getTotal()).toEqual(41872);
+      expect(cart.getTotal().getAmount()).toEqual(41872);
     });
   });
 
@@ -76,7 +76,7 @@ describe('Cart', () => {
       });
 
       expect(cart.summary()).toMatchSnapshot();
-      expect(cart.getTotal()).toBeGreaterThan(0);
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
     });
 
     it('should reset the cart when checkout() is called', () => {
@@ -87,7 +87,7 @@ describe('Cart', () => {
 
       cart.checkout();
 
-      expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
   });
 });
